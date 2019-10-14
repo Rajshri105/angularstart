f5d2534f99dff0900dc11bc5a0da0d4edcb4b7c5
Rajas105 <rajashrichaudhari7@gmail.com>
2019-10-14 17:31:19 +0530

    evenodd

---
+++ first/object.js
@@ -0,0 +1,17 @@
+let obj = {
+    key: "value",
+    age: 10,
+    color: "red",
+    name: "rajas",
+    fn: function () {
+        console.log(`Function Called`)
+    },
+    sadlkfksladf : '989879879',
+    objIn : {
+        ram : '8GB'
+    }
+}
+console.log(obj.age)
+console.log(obj.isAuto)
+obj.fn()
+console.log(obj)
